ac6a9e78a47ca92f64d280153b395ef6242ca336
vkshirsagar6 <vkshirsagar6@gatech.edu>
2026-09-22 06:38:15 -0400

    Discovery-server and order-service are now integrated with inventory-service. The order-service now calls the inventory-service to check if the products are in stock before placing an order. The WebClient is used to make the HTTP request to the inventory-service, and the response is processed to determine if all products are available. If any product is out of stock, an exception is thrown, preventing the order from being placed.

--- order-service/src/main/java/com/tech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/tech/orderservice/service/OrderService.java
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.tech.orderservice.dto.InventoryResponse;
 import com.tech.orderservice.dto.OrderLineItemsDto;
 import com.tech.orderservice.dto.OrderRequest;
-import com.tech.orderservice.dto.InventoryResponse;
 import com.tech.orderservice.model.Order;
 import com.tech.orderservice.model.OrderLineItems;
 import com.tech.orderservice.repository.OrderRepository;
@@ -24,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItems().stream().map(OrderLineItems::getSkuCode).toList();
 
         //call inventory service and place order if product is in stock
-       InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+       InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
